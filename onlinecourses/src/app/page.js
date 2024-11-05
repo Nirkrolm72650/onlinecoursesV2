20e0993633d6050fe2b9860e6cd3847617a62585
Brandon Guyon <dev.brandonguyon@gmail.com>
2024-11-05 16:31:31 +0100

    Add Card component

--- onlinecourses/src/app/page.js
+++ onlinecourses/src/app/page.js
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-
+      <Card />
     </>
 
   );
